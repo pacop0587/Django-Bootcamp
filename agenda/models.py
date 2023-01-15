4876f510523293a15062df994ab7c9c60b32e86c
Paco <pacop0587@gmail.com>
2023-01-14 20:01:06 -0600

    Se agrego models

--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Datos(models.Model):
+    namePerson = models.CharField(max_length=50)
+    emailPerson = models.CharField(max_length=50)
